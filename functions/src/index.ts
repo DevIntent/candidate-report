c037cae1453e06af2d6a3fb272a80aafeac46b73
Michael Prentice <splaktar@gmail.com>
2018-08-25 08:44:34 -0400

    feat: get SSR working in Firebase

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,32 +1,60 @@
-import {renderModuleFactory} from '@angular/platform-server';
+import {enableProdMode} from '@angular/core';
+// Express Engine
+import {ngExpressEngine} from '@nguniversal/express-engine';
+// Import module map for lazy loading
+import {provideModuleMap} from '@nguniversal/module-map-ngfactory-loader';
 import * as express from 'express';
-
+const cors = require('cors')({origin: true});
+const cookieParser = require('cookie-parser')();
 import * as functions from 'firebase-functions';
-import * as fs from 'fs';
-import * as path from 'path';
+import {join} from 'path';
 // noinspection TsLint
-import 'zone.js/dist/zone-node'; // Load zone.js for the server.
+import 'reflect-metadata';
+// noinspection TsLint
+import 'zone.js/dist/zone-node';
 
-// Import the AOT compiled factory for your AppServerModule.
-const AppServerModuleNgFactory = require(path.resolve(__dirname, `../candidate-report/main`)).AppServerModuleNgFactory;
+const {renderModuleFactory} = require('@angular/platform-server');
 
-// Load the index.html file.
-const document = fs.readFileSync(path.resolve(__dirname, `../candidate-report/index.html`), 'utf8');
+const path = require('path');
 
+// Required for Firebase
+(global as any).WebSocket = require('ws');
+(global as any).XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+
+// Faster server renders w/ Prod mode (dev mode never needed)
+enableProdMode();
+
+// Express server
 const app = express();
+app.use(cors);
+app.use(cookieParser);
 
-app.get('**', (req, res) => {
-  const url = req.path;
-  renderModuleFactory(AppServerModuleNgFactory, {document, url})
-    .then(html => {
-      const browserCache = 600; // 5 minutes
-      const cdnCache = 600; // 5 minutes
-      res.set('Cache-Control', `public max-age=${browserCache}, s-maxage=${cdnCache}`);
-      res.send(html);
-    })
-    .catch(err => {
-      console.log(err);
-    });
+const DIST_FOLDER = join(__dirname, '../candidate-report');
+const APP_NAME = 'candidate-report-server';
+
+// * NOTE :: leave this as require() since this file is built Dynamically from webpack
+const {AppServerModuleNgFactory, LAZY_MODULE_MAP} = require(`../${APP_NAME}/main`);
+
+// Our Universal express-engine (found @ https://github.com/angular/universal/tree/master/modules/express-engine)
+app.engine('html', ngExpressEngine({
+  bootstrap: AppServerModuleNgFactory,
+  providers: [
+    provideModuleMap(LAZY_MODULE_MAP)
+  ]
+}));
+
+const index = require('fs')
+  .readFileSync(path.resolve(join(DIST_FOLDER, 'index.html')), 'utf8')
+  .toString();
+
+app.get('**', function(req, res) {
+  renderModuleFactory(AppServerModuleNgFactory, {
+    url: req.path,
+    document: index,
+    extraProviders: [provideModuleMap(LAZY_MODULE_MAP)]
+  }).then(html => res.status(200).send(html));
 });
 
-exports.ssr = functions.https.onRequest(app);
+exports.ssr = functions
+  .region('us-east1')
+  .https.onRequest(app);

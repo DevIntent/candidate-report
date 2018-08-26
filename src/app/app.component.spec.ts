ef04ca809c62e0e5a8cbfb1be2899a913daeb28e
Michael Prentice <splaktar@gmail.com>
2018-08-26 19:34:44 -0400

    fix(tests): update tests, fix vulnerability with old karma
    import modules
    test on firefox

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,14 @@
-import { TestBed, async } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-import { AppComponent } from './app.component';
+import {async, TestBed} from '@angular/core/testing';
+import {MatToolbarModule} from '@angular/material';
+import {RouterTestingModule} from '@angular/router/testing';
+import {AppComponent} from './app.component';
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        MatToolbarModule
       ],
       declarations: [
         AppComponent
@@ -20,12 +23,12 @@ describe('AppComponent', () => {
   it(`should have as title 'candidate-report'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('candidate-report');
+    expect(app.title).toEqual('Candidate Report');
   }));
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to candidate-report!');
+    expect(compiled.querySelector('h1').textContent).toContain('Coming soon');
   }));
 });

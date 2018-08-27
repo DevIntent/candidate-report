aec73ea90948e95e9c8927a97ea4875af9a43b81
Michael Prentice <splaktar@gmail.com>
2018-08-27 06:55:07 -0400

    build(gae): configure deployment for GAE standard

--- server.ts
+++ server.ts
@@ -1,3 +1,6 @@
+import 'reflect-metadata';
+import 'zone.js/dist/zone-node';
+
 import {enableProdMode} from '@angular/core';
 // Express Engine
 import {ngExpressEngine} from '@nguniversal/express-engine';
@@ -6,12 +9,12 @@ import {provideModuleMap} from '@nguniversal/module-map-ngfactory-loader';
 
 import * as express from 'express';
 import {join} from 'path';
-import 'reflect-metadata';
-import 'zone.js/dist/zone-node';
 
 // Required for Firebase
 (global as any).WebSocket = require('ws');
-(global as any).XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+(global as any).XMLHttpRequest = require('xhr2');
+(global as any).XMLHttpRequestEventTarget = require('xhr2').XMLHttpRequestEventTarget;
+(global as any).XMLHttpRequestUpload = require('xhr2').XMLHttpRequestUpload;
 
 // Faster server renders w/ Prod mode (dev mode never needed)
 enableProdMode();
@@ -20,7 +23,7 @@ enableProdMode();
 const app = express();
 
 const PORT = process.env.PORT || 4000;
-const DIST_FOLDER = join(process.cwd(), 'functions/dist');
+const DIST_FOLDER = join(process.cwd(), 'dist');
 const APP_DIR = 'browser';
 
 // * NOTE :: leave this as require() since this file is built Dynamically from webpack

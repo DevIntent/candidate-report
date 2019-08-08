332d5aee57809a2eb5bfca0391358467295fcb52
Michael Prentice <splaktar@gmail.com>
2019-08-08 01:38:10 -0400

    build: update dependencies. fix @nguniversal breaking changes
    remove @angular/flex-layout
    better tune router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,8 @@
 import {NgModule} from '@angular/core';
-import {FlexLayoutModule} from '@angular/flex-layout';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {BrowserModule} from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {RouterModule} from '@angular/router';
+import {PreloadAllModules, RouterModule} from '@angular/router';
 import {AppRoutes} from './app-routes';
 import {AppComponent} from './app.component';
 
@@ -13,9 +12,14 @@ import {AppComponent} from './app.component';
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
-    RouterModule.forRoot(AppRoutes),
+    RouterModule.forRoot(AppRoutes, {
+      useHash: false,
+      preloadingStrategy: PreloadAllModules,
+      scrollPositionRestoration: 'enabled',
+      anchorScrolling: 'enabled',
+      relativeLinkResolution: 'corrected'
+    }),
     BrowserAnimationsModule,
-    FlexLayoutModule,
     MatToolbarModule
   ],
   providers: [],

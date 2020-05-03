adad1ebc3115a73deda101d3b94127903180b0f8
Michael Prentice <splaktar@gmail.com>
2020-05-03 00:25:08 -0400

    fix(server): account for breaking change in @nestjs/ng-universal

--- server/app.module.ts
+++ server/app.module.ts
@@ -1,6 +1,7 @@
 import {Module} from '@nestjs/common';
 import {AngularUniversalModule, applyDomino} from '@nestjs/ng-universal';
 import {join} from 'path';
+import {AppServerModule} from '../src/main.server';
 import {AppController} from './app.controller';
 
 const BROWSER_DIR = join(process.cwd(), 'dist/candidate-report');
@@ -9,8 +10,8 @@ applyDomino(global, join(BROWSER_DIR, 'index.html'));
 @Module({
   imports: [
     AngularUniversalModule.forRoot({
-      viewsPath: BROWSER_DIR,
-      bundle: require('../candidate-report-server/main.js')
+      bootstrap: AppServerModule,
+      viewsPath: BROWSER_DIR
     }),
   ],
   controllers: [AppController],
